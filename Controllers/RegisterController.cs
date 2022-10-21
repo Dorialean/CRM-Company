52dd8b1a8d76d76668fd6945be6662e4e3a3d64b
Doriealean <lana.lakutina@mail.ru>
2022-10-21 15:55:35 +0300

    register form

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,3 +1,4 @@
+using Company_CRM.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company_CRM.Controllers;
@@ -7,6 +8,20 @@ public class RegisterController : Controller
     // GET
     public IActionResult Index()
     {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(Employee empl)
+    {
+        Console.WriteLine(empl.FirstName);
+        Console.WriteLine(empl.SecondName);
+        Console.WriteLine(empl.FatherName);
+        Console.WriteLine(empl.Address);
+        Console.WriteLine(empl.Phone);
+        Console.WriteLine(empl.Email);
+        Console.WriteLine(empl.Login);
+        Console.WriteLine(empl.Password);
+        Console.WriteLine(empl.FactoryRole);
         return View();
     }
 }
