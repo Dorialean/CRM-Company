39545bf8bc5828640b7a901681eb85edb29d23e2
Dorialean <lana.lakutina@mail.ru>
2022-10-30 19:40:50 +0300

    Template for clients and employees

---
+++ Controllers/WorkController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company_CRM.Controllers
+{
+    public class WorkController : Controller
+    {
+        [Authorize]
+        public IActionResult EmployeeSpace()
+        {
+            return View();
+        }
+
+        public IActionResult ClientOrder()
+        {
+            return View();
+        }
+    }
+}
