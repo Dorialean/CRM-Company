8bc0fa985ca13ae152d7e22ab25990ac5e16d265
Dorialean <lana.lakutina@mail.ru>
2022-10-31 18:01:34 +0300

    #15 client can create contracts

--- Controllers/WorkController.cs
+++ Controllers/WorkController.cs
@@ -1,19 +1,101 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Company_CRM.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company_CRM.Controllers
 {
     public class WorkController : Controller
     {
+        private readonly SneakerFactoryContext _sneakerFactoryContext;
+
+        public WorkController(SneakerFactoryContext sneakerFactoryContext)
+        {
+            _sneakerFactoryContext = sneakerFactoryContext;
+        }
+
         [Authorize]
+        [HttpGet]
         public IActionResult EmployeeSpace()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult ClientOrder()
         {
+            ViewBag.Sneakers = _sneakerFactoryContext.Sneakers.ToArray();
             return View();
+        }
+        [HttpPost]
+        public IActionResult ClientOrder(ClientOrderInfo info)
+        {
+            Console.WriteLine(info);
+            using (_sneakerFactoryContext)
+            {
+                var avClient = new AvailableClient()
+                {
+                    FirstName = info.FirstName,
+                    SecondName = info.SecondName,
+                    OrganisationName = info.OrganisationName,
+                    Phone = info.Phone,
+                    Email = info.Email,
+                    Address = info.Address,
+                };
+                var pClient = new PotentialClient()
+                {
+                    FirstName = info.FirstName,
+                    SecondName = info.SecondName,
+                    OrganisationName = info.SecondName,
+                    Phone = info.Phone,
+                    Email = info.Email,
+                    Address = info.Address,
+                    Meeting = info.Meeting
+                };
+
+                if (_sneakerFactoryContext.AvailableClients.FirstOrDefault(ac => ac.OrganisationName == avClient.OrganisationName) == null)
+                    _sneakerFactoryContext.AvailableClients.Add(avClient);
+                if (_sneakerFactoryContext.PotentialClients.FirstOrDefault(pc => pc.OrganisationName == pClient.OrganisationName) == null)
+                    _sneakerFactoryContext.PotentialClients.Add(pClient);
+                _sneakerFactoryContext.SaveChanges();
+
+                int idForContract = _sneakerFactoryContext.AvailableClients.FirstOrDefault(c => c.OrganisationName == info.OrganisationName).ClientId;
+                List<Contract> contracts = new();
+                int contractsId = _sneakerFactoryContext.Contracts.Count();
+                foreach (var sneakerId in info.SneakerIdToValue.Keys)
+                {
+                    int amount = int.Parse(info.SneakerIdToValue[sneakerId]);
+                    if(amount > 0)
+                    {
+                        for (int i = 0; i < amount; i++)
+                        {
+                            var contract = new Contract()
+                            {
+                                ClientId = idForContract,
+                                SneakerId = _sneakerFactoryContext.Sneakers.First(s => s.SneakerId == int.Parse(sneakerId)).SneakerId,
+                                SignDate = DateTime.Now,
+                                Deadline = info.ContractInfo.Deadline
+                            };
+                            contracts.Add(contract);
+                        }
+                        
+                    }
+                }
+                _sneakerFactoryContext.Contracts.AddRange(contracts);
+                _sneakerFactoryContext.SaveChanges();
+                return RedirectToAction("ClientOrderResult", "Work", _sneakerFactoryContext.AvailableClients.First(ac => ac.OrganisationName == info.OrganisationName) );
+            }
+            
+        }
+        
+        public IActionResult ClientOrderResult(AvailableClient client)
+        {
+            using (_sneakerFactoryContext)
+            {
+                var contracts = _sneakerFactoryContext.Contracts.Where(c => c.ClientId == client.ClientId).Select(x => x).ToArray();
+                var sneakers = _sneakerFactoryContext.Sneakers.ToArray();
+                return View(new ClientContracts() { Contracts = contracts, Sneakers = sneakers });
+            }
+            
         }
     }
 }
---
+++ Models/ClientContracts.cs
@@ -0,0 +1,8 @@
+﻿namespace Company_CRM.Models
+{
+    public class ClientContracts
+    {
+        public Contract[] Contracts { get; set; }
+        public Sneaker[] Sneakers { get; set; }
+    }
+}
