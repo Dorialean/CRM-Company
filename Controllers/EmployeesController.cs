7983e3e6e9f2a094c261d8efcbf23f5ae9b754fb
Dorialean <lana.lakutina@mail.ru>
2022-11-03 20:48:45 +0300

    need to correctly parse all strings

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,81 @@
+﻿using Company_CRM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using Npgsql;
+using System.Runtime.InteropServices;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Company_CRM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string connectionString;
+
+        public EmployeesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                connectionString = configuration.GetConnectionString("LaptopConnectionString");
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                connectionString = configuration.GetConnectionString("WindowsConntectionString");
+            else
+                connectionString = configuration.GetConnectionString("MacConntectionString");
+        }
+
+
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string json;
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                string query = "SELECT * FROM get_reports();";
+                using (var cmd = new NpgsqlCommand(query, conn))
+                {
+                    NpgsqlDataReader emplQueryRes = await cmd.ExecuteReaderAsync();
+
+                    var r = Serialize(emplQueryRes);
+                    json = JsonConvert.SerializeObject(r, Formatting.Indented);
+                }
+                return new JsonResult(json);
+            }
+
+            IEnumerable<Dictionary<string, object>> Serialize(NpgsqlDataReader reader)
+            {
+                var results = new List<Dictionary<string, object>>();
+                var cols = new List<string>();
+                for (var i = 0; i < reader.FieldCount; i++)
+                    cols.Add(reader.GetName(i));
+
+                while (reader.Read())
+                    results.Add(SerializeRow(cols, reader));
+
+                return results;
+            }
+
+
+            Dictionary<string, object> SerializeRow(IEnumerable<string> cols,
+                                                NpgsqlDataReader reader)
+            {
+                var result = new Dictionary<string, object>();
+                foreach (var col in cols)
+                    result.Add(col, reader[col]);
+                return result;
+            }
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ wwwroot/js/EmployeeReport.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    let employeeButton = $("#job_report");
+
+    employeeButton.click(function () {
+        fetch('https://localhost:7265/api/employees')
+            .then(resp => resp.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = 'jobs.json';
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+                alert('your file.jobs has downloaded!');
+            })
+            .catch(() => alert('oh no!'))
+    });
+});
