fd99d0950723ee491b9d82834804898f116a0bb2
Dorialean <lana.lakutina@mail.ru>
2022-11-04 18:45:50 +0300

    #17 client search ready

---
+++ wwwroot/js/ClientSearcher.js
@@ -0,0 +1,33 @@
+﻿$(function () {   
+    let clientSearchButton = $("#client_searchb");
+    let clientSerachBox = $("#client_search_info");
+    let clientSearchRes = $("#client_search_res");
+
+    clientSearchButton.click(() => {
+        let info = clientSerachBox.val();
+        console.log(allClients);
+        clientSearchRes.html("");
+        allClients.forEach((client) => {
+            let addInfo = () => {
+                clientSearchRes.append("<div class='row alert alert-warning mt-1 mb-1'>"
+                    + "<p class='row'>Название организации: " + client.organisationName + "</p>"
+                    + "<p class='row'>Номер контракта: " + client.contractId + "</p>"
+                    + "<p class='row'>Имя: " + client.firstName + "</p>"
+                    + "<p class='row'>Фамилия: " + client.secondName + "</p>"
+                    + "<p class='row'>Почта: " + client.email + "</p>"
+                    + "<p class='row'>Телефон: " + client.phone + "</p>"
+                    + "<p class='row'>Адрес: " + client.address + "</p>" + "</div>")
+            };
+            if (!info) {
+                info = null;
+            }
+
+            if (client.clientId == info) {
+                addInfo();
+            }
+            else if (client.organisationName.includes(info)) {
+                addInfo();
+            }
+        })
+    });
+});
