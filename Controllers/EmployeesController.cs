32f951aa28f471232432ea7be650388380cd1812
Dorialean <lana.lakutina@mail.ru>
2022-11-04 16:18:06 +0300

    all reports ready

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,9 +1,12 @@
 ﻿using Company_CRM.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using Newtonsoft.Json;
+using System.Text.Json;
 using Npgsql;
+using NuGet.Protocol;
+using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,52 +33,32 @@ namespace Company_CRM.Controllers
 
         // GET: api/<EmployeeController>
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<IEnumerable<EmployeeReport>>> Get()
         {
-            string json;
+            List<EmployeeReport> report = new();
             using (var conn = new NpgsqlConnection(connectionString))
             {
                 await conn.OpenAsync();
                 string query = "SELECT * FROM get_reports();";
                 using (var cmd = new NpgsqlCommand(query, conn))
                 {
-                    NpgsqlDataReader emplQueryRes = await cmd.ExecuteReaderAsync();
-                    //Выдаёт неправильно спаршенные строки
-                    var r = Serialize(emplQueryRes);
-                    json = JsonConvert.SerializeObject(r, Formatting.Indented);
+                    var emplQueryRes = await cmd.ExecuteReaderAsync();
+                    
+                    while(await emplQueryRes.ReadAsync())
+                    {
+                        report.Add(new EmployeeReport()
+                        {
+                            EmployeeId = emplQueryRes.GetInt32(0),
+                            AllTasks = emplQueryRes.GetInt32(1),
+                            InTimeCompleted = emplQueryRes.GetInt32(2),
+                            NotInTimeCompleted = emplQueryRes.GetInt32(3),
+                            FailedTasks = emplQueryRes.GetInt32(4),
+                            InWorkTasks = emplQueryRes.GetInt32(5)
+                        });
+                    }
                 }
-                return new JsonResult(json);
+                return report;
             }
-
-            IEnumerable<Dictionary<string, object>> Serialize(NpgsqlDataReader reader)
-            {
-                var results = new List<Dictionary<string, object>>();
-                var cols = new List<string>();
-                for (var i = 0; i < reader.FieldCount; i++)
-                    cols.Add(reader.GetName(i));
-
-                while (reader.Read())
-                    results.Add(SerializeRow(cols, reader));
-
-                return results;
-            }
-
-
-            Dictionary<string, object> SerializeRow(IEnumerable<string> cols,
-                                                NpgsqlDataReader reader)
-            {
-                var result = new Dictionary<string, object>();
-                foreach (var col in cols)
-                    result.Add(col, reader[col]);
-                return result;
-            }
-        }
-
-        // GET api/<EmployeeController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
         }
     }
 }
---
+++ wwwroot/js/Reports.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+    let jobButton = $("#job_report");
+    let employeeButton = $("#employee_report");
+
+    jobButton.click(function() {
+        fetch('https://localhost:7265/api/jobs')
+        .then(resp => resp.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'jobs.json';
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            alert('your file: jobs has downloaded!');
+        })
+        .catch(() => alert('oh no!'))
+    });
+
+    employeeButton.click(function () {
+        fetch('https://localhost:7265/api/employees')
+            .then(resp => resp.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = 'employees.json';
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+                alert('your file: employees has downloaded!');
+            })
+            .catch(() => alert('oh no!'))
+    });
+});
